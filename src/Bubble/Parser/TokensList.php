bdc722b79e5debc6269ab5306e9517676af4dc16
Nana Axel <ax.lnana@outlook.com>
2018-04-01 17:32:17 +0100

    Update license and change version number

--- src/Bubble/Parser/TokensList.php
+++ src/Bubble/Parser/TokensList.php
@@ -3,26 +3,30 @@
 /**
  * Bubble - A PHP template engine
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  *
  * @category  Library
  * @package   Bubble
  * @author    Axel Nana <ax.lnana@outlook.com>
  * @copyright 2018 Aliens Group, Inc.
- * @license   LGPL-3.0 <https://opensource.org/licenses/LGPL-3.0>
- * @version   GIT: 0.0.1
+ * @license   MIT <https://github.com/na2axl/bubble/blob/master/LICENSE>
+ * @version   GIT: 1.1.0
  * @link      http://bubble.na2axl.tk
  */
 
@@ -40,7 +44,7 @@ use Bubble\Tokens\ForeachToken;
  * @category Parser
  * @package  Bubble
  * @author   Axel Nana <ax.lnana@outlook.com>
- * @license  LGPL-3.0 <https://opensource.org/licenses/LGPL-3.0>
+ * @license  MIT <https://github.com/na2axl/bubble/blob/master/LICENSE>
  * @link     http://bubble.na2axl.tk/docs/api/Bubble/Parser/Tokenizer
  * @final
  */
